861c8fa5c550ee726ca30b99a2aa42b9e29ed1c3
trak2016z <trak2016z@gmail.com>
2016-11-17 00:11:07 +0100

    commit1

---
+++ app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angular2FundamentalsG5App
+ * @description
+ * # angular2FundamentalsG5App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angular2FundamentalsG5App', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
